7cd3e8d06c9a63313b9e6b23107d30b329f12b82
Pedro Guananja <pguananja@alegramed.com>
2024-07-28 13:58:37 -0300

    Add project files.

---
+++ apiConsultorio/Models/Entities/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace apiConsultorio.Models.Entities;
+
+public class EntityBase<T>
+{
+    [Key]
+    public T Id { get; set; }
+    public int CreatedBy { get; set; }
+    public int? UpdatedBy { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+}
